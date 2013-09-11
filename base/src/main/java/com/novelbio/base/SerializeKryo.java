25d271d8b5c2f82b0ca924082198d59423a35a1d
zong0jie <zong0jie@gmail.com>
2013-09-11 20:14:19 +0800

    1. 序列化添加线程锁

--- base/src/main/java/com/novelbio/base/SerializeKryo.java
+++ base/src/main/java/com/novelbio/base/SerializeKryo.java
@@ -3,18 +3,18 @@ package com.novelbio.base;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
-import com.novelbio.base.fileOperate.FileOperate;
 
+/** 内部有synchronized*/
 public class SerializeKryo {
 	Kryo kryo = new Kryo();
 	
-	public byte[] write(Object object) {
+	public synchronized byte[] write(Object object) {
         Output output = new Output(100, -1);
         kryo.writeClassAndObject(output, object);
         byte[] data = output.getBuffer();
         return data;
 	}
-	public Object read( byte[] data) {
+	public synchronized Object read( byte[] data) {
         Input input = new Input();
  		input.setBuffer(data);
  		return kryo.readClassAndObject(input);
