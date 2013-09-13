503bb609fd499836b882f1df240501aba3d72e53
zong0jie <zong0jie@gmail.com>
2013-09-13 12:21:48 +0800

    1. 上传sam文件操作相关的类

---
+++ base/src/main/java/net/sf/samtools/SeekableHDFSstream.java
@@ -0,0 +1,78 @@
+package net.sf.samtools;
+
+import java.io.IOException;
+
+import net.sf.samtools.seekablestream.SeekableStream;
+
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.log4j.Logger;
+
+import com.novelbio.base.fileOperate.FileHadoop;
+
+/**
+ * 将HDFS的流包装成Samtools识别的Seekable流
+ * @author zong0jie
+ *
+ */
+public class SeekableHDFSstream extends SeekableStream {
+	
+	private static final Logger logger = Logger.getLogger(SeekableHDFSstream.class);
+	
+	FileHadoop fileHadoop;
+	FSDataInputStream fsDataInputStream;
+	long fileLength;
+	
+	public SeekableHDFSstream(FileHadoop fileHadoop) {
+		this.fileHadoop = fileHadoop;
+		this.fsDataInputStream = fileHadoop.getInputStream();
+		fileLength = fileHadoop.getContentSummary().getLength();
+	}
+	
+	@Override
+	public long length() {
+		return fileHadoop.getContentSummary().getLength();
+	}
+
+	@Override
+	public long position() throws IOException {
+		return fsDataInputStream.getPos();
+	}
+
+	@Override
+	public void seek(long position) throws IOException {
+		fsDataInputStream.seek(position);
+	}
+	
+	@Override
+	public int read(byte[] buffer, int offset, int length) throws IOException {
+		return fsDataInputStream.read(buffer, offset, length);
+	}
+	
+	public void readFully(byte[] buffer) throws IOException {
+		fsDataInputStream.readFully(buffer);
+	}
+	
+	@Override
+	public void close() throws IOException {
+		fsDataInputStream.close();
+	}
+
+	@Override
+	public boolean eof() throws IOException {
+		return fileLength == fsDataInputStream.getPos();
+	}
+	
+	/**
+	 * 在sam-1.87版本中，仅用来显示报错信息
+	 */
+	@Override
+	public String getSource() {
+		return fileHadoop.getName();
+	}
+
+	@Override
+	public int read() throws IOException {
+		return fsDataInputStream.read();
+	}
+
+}
--- base/src/test/java/SystemInfo.java
+++ base/src/test/java/SystemInfo.java
@@ -1,24 +1,45 @@
+import org.hyperic.sigar.Cpu;
+import org.hyperic.sigar.CpuInfo;
+import org.hyperic.sigar.CpuPerc;
 import org.hyperic.sigar.Mem;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
 import org.hyperic.sigar.Swap;
 
 public class SystemInfo {
-	public static void main(String[] args) throws SigarException {
+	public static void main(String[] args) {
 		Sigar sigar = new Sigar();
-		Mem mem = sigar.getMem();
+		Mem mem = null;
+		try {
+			mem = sigar.getMem();
+			CpuInfo  info = sigar.getCpuInfoList()[0];    
+			CpuPerc perc = sigar.getCpuPerc();    
+			Cpu timer = sigar.getCpu();    
+			
+			System.out.println(info.getModel());
+			System.out.println(perc.toString());
+			System.out.println(timer.toString());
+			System.out.println((int)perc.getCombined());
+			
+			
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+		}
 		// 内存总量
 		System.out.println("内存总量:    " + mem.getTotal() / 1024L + "K av");
 		// 当前内存使用量
 		System.out.println("当前内存使用量:    " + mem.getUsed() / 1024L + "K used");
 		// 当前内存剩余量
-		Swap swap = sigar.getSwap();
-		// 交换区总量
-		System.out.println("交换区总量:    " + swap.getTotal() / 1024L + "K av");
-		// 当前交换区使用量
-		System.out.println("当前交换区使用量:    " + swap.getUsed() / 1024L + "K used");
-		// 当前交换区剩余量
-		System.out.println("当前交换区剩余量:    " + swap.getFree() / 1024L + "K free");
+//		Swap swap = sigar.getSwap();
+//		// 交换区总量
+//		System.out.println("交换区总量:    " + swap.getTotal() / 1024L + "K av");
+//		// 当前交换区使用量
+//		System.out.println("当前交换区使用量:    " + swap.getUsed() / 1024L + "K used");
+//		// 当前交换区剩余量
+//		System.out.println("当前交换区剩余量:    " + swap.getFree() / 1024L + "K free");
 	}
 
 
