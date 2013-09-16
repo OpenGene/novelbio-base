d0caf4826e70615fa8912531b190b7d198182fc3
zong0jie <zong0jie@gmail.com>
2013-09-16 20:20:05 +0800

    1. SepSign添加了一个分隔符类型

--- base/src/main/java/com/novelbio/base/SepSign.java
+++ base/src/main/java/com/novelbio/base/SepSign.java
@@ -22,6 +22,16 @@ public class SepSign {
 	public static final String SEP_INFO = "@@";
 	
 	/**
+	 * <b>不要往这个symbol中添加"/"或"\"</b><br>
+	 * 中间等级
+	 * 分割 NCBIID的title和内容
+	 * 如NCBI@@protein coding
+	 * "(+)"
+	 */
+	public static final String SEP_INFO2 = "(+-)";
+	
+	
+	/**
 	 * 最低等级
 	 * 分割同一个数据库的两个不同的注释信息
 	 * "#/#"
--- base/src/test/java/SystemInfo.java
+++ base/src/test/java/SystemInfo.java
@@ -1,3 +1,7 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+
 import org.hyperic.sigar.Cpu;
 import org.hyperic.sigar.CpuInfo;
 import org.hyperic.sigar.CpuPerc;
@@ -7,7 +11,7 @@ import org.hyperic.sigar.SigarException;
 import org.hyperic.sigar.Swap;
 
 public class SystemInfo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		Sigar sigar = new Sigar();
 		Mem mem = null;
 		try {
@@ -16,7 +20,7 @@ public class SystemInfo {
 			CpuPerc perc = sigar.getCpuPerc();    
 			Cpu timer = sigar.getCpu();    
 			
-			System.out.println(info.getModel());
+			System.out.println(info.getTotalCores());
 			System.out.println(perc.toString());
 			System.out.println(timer.toString());
 			System.out.println((int)perc.getCombined());
@@ -31,7 +35,9 @@ public class SystemInfo {
 		// 内存总量
 		System.out.println("内存总量:    " + mem.getTotal() / 1024L + "K av");
 		// 当前内存使用量
-		System.out.println("当前内存使用量:    " + mem.getUsed() / 1024L + "K used");
+		System.out.println("当前内存使用量:    " + mem.getActualUsed() / 1024L + "K used");
+		System.out.println("当前内存空闲量:    " + mem.getActualFree() / 1024L + "K used");
+		System.out.println("当前内存空闲量:    " + Runtime.getRuntime().totalMemory() + "K used");
 		// 当前内存剩余量
 //		Swap swap = sigar.getSwap();
 //		// 交换区总量
@@ -40,6 +46,15 @@ public class SystemInfo {
 //		System.out.println("当前交换区使用量:    " + swap.getUsed() / 1024L + "K used");
 //		// 当前交换区剩余量
 //		System.out.println("当前交换区剩余量:    " + swap.getFree() / 1024L + "K free");
+		
+        Map<String, String> map = System.getenv();  
+        String userName = map.get("USERNAME");// 获取用户名  
+        String computerName = map.get("COMPUTERNAME");// 获取计算机名  
+        String userDomain = map.get("USERDOMAIN");// 获取计算机域名 
+        System.out.println(userName);
+        System.out.println(computerName);
+        System.out.println(userDomain);
+        System.out.println(InetAddress.getLocalHost().getHostName());
 	}
 
 
