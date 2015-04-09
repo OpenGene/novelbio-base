e08209c4e814ab24f8cfee0a7921e73fc41f35ae
Zong Jie <zongjie@novelbio.com>
2015-04-09 16:51:05 +0800

    1. 删除自己写的Hadoop的一个类

--- base/src/test/java/SystemInfo.java
+++ base/src/test/java/SystemInfo.java
@@ -1,24 +1,10 @@
-import java.net.InetAddress;
+import java.io.File;
 import java.net.UnknownHostException;
-import java.util.Map;
-
-import org.hyperic.sigar.Cpu;
-import org.hyperic.sigar.CpuInfo;
-import org.hyperic.sigar.CpuPerc;
-import org.hyperic.sigar.Mem;
-import org.hyperic.sigar.Sigar;
-import org.hyperic.sigar.SigarException;
-import org.hyperic.sigar.Swap;
-
-import com.novelbio.base.dataOperate.TxtReadandWrite;
 
 public class SystemInfo {
 	public static void main(String[] args) throws UnknownHostException {
-		TxtReadandWrite txtRead = new TxtReadandWrite("/home/novelbio/software/hadoop/配置文件/172/yarn-site.xml");
-		for (String string : txtRead.readlines()) {
-			System.out.println(string);
-		}
-		
+		File file = new File("/hdfs:/nbCloud/testJava/NBCplatform/testDNAmap/bwaResult/resultBWA5_sorted.bam22.bai");
+		System.out.println(file.length());
 	}
 
 
