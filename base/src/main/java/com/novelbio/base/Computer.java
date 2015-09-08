2984ea178e686f14b085bd4217fe55825c2120c6
Zong Jie <zongjie@novelbio.com>
2015-09-08 22:59:30 +0800

    1. 删除computer中的cpu和内存监控

--- base/src/main/java/com/novelbio/base/Computer.java
+++ base/src/main/java/com/novelbio/base/Computer.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.hyperic.sigar.Mem;
-import org.hyperic.sigar.Sigar;
-import org.hyperic.sigar.SigarException;
+//import org.hyperic.sigar.Mem;
+//import org.hyperic.sigar.Sigar;
+//import org.hyperic.sigar.SigarException;
 
 import com.novelbio.base.cmd.CmdOperate;
 import com.novelbio.base.dataStructure.PatternOperate;
@@ -25,7 +25,6 @@ public class Computer implements Serializable {
 	private static final Logger logger = Logger.getLogger(Computer.class);
 	private static final long serialVersionUID = 1943457765445L;
 	static final int minRunTaskMem = 10;//单位为MB 
-	static transient Sigar sigar;
 	protected transient static int memToBeUsed = 5000;
 	protected transient static int cpuToBeUsed = 2;
 	
@@ -33,36 +32,36 @@ public class Computer implements Serializable {
 	protected String ip;
 	/** 计算机的名字 如:slaver1、master1.. */
 	protected String name;
-	protected int cpuCoreNum;
-	/** 总内存数，以MB为单位 */
-	protected int memoryAll;
-
-	
-	/** 数据库里记载的空闲cpu数量 */
-	protected int cpuCoreFreeInDB;
-	/** 实际空间cpu数量 */
-	protected int cpuCoreFreeReal;
-	/** 数据库里记载的空闲内存数量 */
-	protected int memoryFreeDB;
-	/** 实际空闲内存数量 */
-	protected int memoryFreeReal;
+//	protected int cpuCoreNum;
+//	/** 总内存数，以MB为单位 */
+//	protected int memoryAll;
+//
+//	
+//	/** 数据库里记载的空闲cpu数量 */
+//	protected int cpuCoreFreeInDB;
+//	/** 实际空间cpu数量 */
+//	protected int cpuCoreFreeReal;
+//	/** 数据库里记载的空闲内存数量 */
+//	protected int memoryFreeDB;
+//	/** 实际空闲内存数量 */
+//	protected int memoryFreeReal;
 	
 	
 	protected Computer() {}
 	
 	public void initial() {
-		if (sigar == null) {
-			sigar = new Sigar();
-		}
-		try {
-			Mem mem = sigar.getMem();
-			memoryAll = (int) (mem.getTotal() / (1024L*1024L));
-			cpuCoreNum = sigar.getCpuInfoList()[0].getTotalCores();
-			memoryFreeDB = (int) (mem.getActualFree() / (1024L*1024L)) - memToBeUsed;
-			cpuCoreFreeInDB = cpuCoreNum - cpuToBeUsed;
-		} catch (SigarException e) {
-			e.printStackTrace();
-		}
+//		if (sigar == null) {
+//			sigar = new Sigar();
+//		}
+//		try {
+//			Mem mem = sigar.getMem();
+//			memoryAll = (int) (mem.getTotal() / (1024L*1024L));
+//			cpuCoreNum = sigar.getCpuInfoList()[0].getTotalCores();
+//			memoryFreeDB = (int) (mem.getActualFree() / (1024L*1024L)) - memToBeUsed;
+//			cpuCoreFreeInDB = cpuCoreNum - cpuToBeUsed;
+//		} catch (SigarException e) {
+//			e.printStackTrace();
+//		}
 		try {
 			InetAddress[] IP = InetAddress.getAllByName(InetAddress.getLocalHost().getHostName());
 			for (InetAddress inetAddress : IP) {
@@ -145,37 +144,6 @@ public class Computer implements Serializable {
 	public void setIp(String ip) {
 		this.ip = ip;
 	}
-	/** 获得当前cpu和内存的信息 */
-	public void setCpuMemInfo() {
-		try {
-			Mem mem = sigar.getMem();
-			memoryFreeReal = (int) (mem.getActualFree() / (1024L*1024L));
-		} catch (SigarException e) {
-			e.printStackTrace();
-			memoryFreeReal = -1;
-		}
-		try {
-			cpuCoreFreeReal = (int) (sigar.getCpuPerc().getIdle()*cpuCoreNum);
-		} catch (SigarException e) {
-			e.printStackTrace();
-			cpuCoreFreeReal = -1;
-		}
-	}
-	
-	public long getMemoryAll() {
-		return memoryAll;
-	}
-	
-	/**
-	 * <b>用之前先调用{@link #getCpuMemInfo()}</b><br>
-	 * 实际内存使用
-	 * MB为单位<p>
-	 * 返回负数表示出错
-	 */
-	public int getMemoryFreeReal() {
-		return memoryFreeReal;
-	}
-	
 	public String getIp() {
 		return ip;
 	}
@@ -189,67 +157,9 @@ public class Computer implements Serializable {
 		String ip = getIp();
 		return name + SepSign.SEP_INFO_SIMPLE + ip.replace(".", "_");
 	}
-
-	/** 设定数据库记录的空闲内存大小，每个task有消耗的内存数，根据该数据减去内存大小，单位是MB */
-	public void setMemoryFreeDB(int memoryFreeDB) {
-		this.memoryFreeDB = memoryFreeDB;
-	}
-	/** 获得数据库的内存大小，单位是MB */
-	public long getMemoryFreeDB() {
-		return memoryFreeDB;
-	}
-	/** 设定数据库记录的空闲cpu核心数，每个task有消耗的cpu核心数，根据该数据减去cpu核心数量 */
-	public void setCpuCoreFreeInDB(int cpuCoreFreeInDB) {
-		this.cpuCoreFreeInDB = cpuCoreFreeInDB;
-	}
-	/** 本机在数据库，也就是task消耗下，理论CPU空闲数 */
-	public int getCpuCoreFreeInDB() {
-		return cpuCoreFreeInDB;
-	}
-	/** 
-	 * <b>用之前先调用{@link #getCpuMemInfo()}</b><br>
-	 * 本机实际有多少CPU核心空闲<br>
-	 * 小于0则报错
-	 * @return
-	 */
-	public int getCpuCoreFreeReal() {
-		return cpuCoreFreeReal;
-	}
-	/** 本机有多少CPU核心 */
-	public int getCpuCoreNum() {
-		return cpuCoreNum;
-	}
-
-	/**
-	 * cpu使用百分比
-	 */
-	public double getPerCpu(){
-		return  100 -Math.rint(cpuCoreFreeReal*100/cpuCoreNum );
-	}
-	
-	/**
-	 * cpuDB使用百分比
-	 */
-	public double getPerCpuDb(){
-		return  100 -Math.rint(cpuCoreFreeInDB*100/cpuCoreNum );
-	}
-	
-	/**
-	 * 内存使用百分比
-	 */
-	public double getPerMemery() {
-	return 100 - Math.rint(memoryFreeReal*100/memoryAll);
-	}
-	
-	/**
-	 * 内存使用百分比(DB)
-	 */
-	public double getPerMemeryDb() {
-	return 100 - Math.rint(memoryFreeDB*100/memoryAll);
-	}
 		
 	private String getComputerInfo() {
-		return getIp() + SepSign.SEP_ID + getName() + SepSign.SEP_ID + getCpuCoreNum();
+		return getIp() + SepSign.SEP_ID + getName();
 	}
 	
 	public boolean equals(Object obj) {
