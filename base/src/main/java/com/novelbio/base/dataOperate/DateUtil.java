fc914e51f7f1d763f42874b45f492e6a1f4b4851
fanxiaolong <fanxiaolong@novelbio.com>
2015-12-17 09:09:47 +0800

    添加一个日期比较方法.

--- base/src/main/java/com/novelbio/base/dataOperate/DateUtil.java
+++ base/src/main/java/com/novelbio/base/dataOperate/DateUtil.java
@@ -221,4 +221,23 @@ public class DateUtil {
 	     return formatDate.format(currentDate); //将日期时间格式化
 	}
 	
+	/**
+	 * 计算两个日期之间相差的天数
+	 * 
+	 * @param smdate　较小的时间
+	 * @param bdate　较大的时间
+	 * @return 相差天数
+	 * @throws ParseException
+	 */
+	public static int daysBetween(Date smdate, Date bdate){
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(smdate);
+		long time1 = cal.getTimeInMillis();
+		cal.setTime(bdate);
+		long time2 = cal.getTimeInMillis();
+		long between_days = (time2 - time1) / (1000 * 3600 * 24);
+
+		return Integer.parseInt(String.valueOf(between_days));
+	}
+	
 }
