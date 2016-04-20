8933548aff0098151ad53b6bd48e81f7b0ec9096
fanxiaolong <fanxiaolong@novelbio.com>
2016-04-20 16:10:32 +0800

    持续集成脚本测试.

--- base/src/test/java/com/novelbio/base/TestStringOperate.java
+++ base/src/test/java/com/novelbio/base/TestStringOperate.java
@@ -10,5 +10,7 @@ public class TestStringOperate {
 		String str = "*adCVs*34_a _09_b5*[/435^*&城池()^$$&*).{}+.|.)%%*(*.中国}34{45[]12.fd'*&999下面.是中..文的-字符￥……{}【】。，；’“‘”？";
 		String result = StringOperate.replaceSpecialCode(str);
 		Assert.assertEquals("adCVs34_a _09_b5435城池....中国344512.fd999下面.是中文的-字符", result);
+		
+		Assert.assertTrue(1==2);
 	}
 }
