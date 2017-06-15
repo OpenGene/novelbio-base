5d0ae25e18a147edaaaf659fb81d3b68891b2dba
luwei <luwei@novelbio.com>
2017-06-15 14:05:42 +0800

    添加了新的AES加解密算法

--- base/src/main/java/com/novelbio/base/security/Crypter.java
+++ base/src/main/java/com/novelbio/base/security/Crypter.java
@@ -2,35 +2,41 @@ package com.novelbio.base.security;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.novelbio.base.StringOperate;
+
 public class Crypter {
 	private static Logger logger = LoggerFactory.getLogger(Crypter.class);
-	
+
 	private static char[] hexChar = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-	
+
 	/** RSA公钥 */
 	private static String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCR4nOCkGnBwUcQPzXCYnQwQ2+IXwkTkGDc3WyU\n"
-									+ "8eAlV/jQTO2LGv0JECewGr2g+DetnwAfvHgv4HH3NCAgbK2yUnOpQPSaMZIUMNCWatoNTsZm0WOn\n"
-									+ "3XQtreQdAWgskPflxU5oJs9IUErmHoXzs2aVxozizJPazLIKKttdz5PJlwIDAQAB";
+			+ "8eAlV/jQTO2LGv0JECewGr2g+DetnwAfvHgv4HH3NCAgbK2yUnOpQPSaMZIUMNCWatoNTsZm0WOn\n"
+			+ "3XQtreQdAWgskPflxU5oJs9IUErmHoXzs2aVxozizJPazLIKKttdz5PJlwIDAQAB";
 	/** RSA私钥 */
 	private static String privateKey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAJHic4KQacHBRxA/NcJidDBDb4hf\n"
-									+ "CROQYNzdbJTx4CVX+NBM7Ysa/QkQJ7AavaD4N62fAB+8eC/gcfc0ICBsrbJSc6lA9JoxkhQw0JZq\n"
-									+ "2g1OxmbRY6fddC2t5B0BaCyQ9+XFTmgmz0hQSuYehfOzZpXGjOLMk9rMsgoq213Pk8mXAgMBAAEC\n"
-									+ "gYA9fi/khE0f8AGtdoeJpYiGY5aa7DxeM8iwsOE5M2+hLiDiZNrofPrWHCoukEcImDiYBeK+fepH\n"
-									+ "fBtt8VntcQIR+Fg/4G7yM4T8cF6IoHN8ygjXtfUlen8U0tF5Zoje1/sCnkEtXFcnLT57RPqE+1/k\n"
-									+ "YTb+JlxCRBab+tcuS8j5SQJBAOWdXh+WRHxa9Y0wN3l6KZtbH4ZXMXYwWkR89ezlwMX4epDaVHIg\n"
-									+ "1SonW0x5qyfp1pBsmkO+Ktb9To1v9mJgOrsCQQCipfk0Onmaki/C3/q4guqXNF1jMMHW/dkVg/1L\n"
-									+ "lv1/ttf1pzAYUKwsmeBvCql6dK17lB330kfS2qS2SmTQSATVAkANNWJGuuQxqyHY/18Rk9902mcT\n"
-									+ "2Uw1Gk73BaE4AXd3a/XRA148OntIs37jBVS9NQxsvnKZVwUr3OJ57Gjl+9clAkAkb/5mPvtjL4DX\n"
-									+ "rYnjsCCSAz8wq4mhenkZotoqBwd/hxzWgb/6kogf7dEjz0Wsk1sSJlqYXFmO2UCxPfVb2+aJAkB2\n"
-									+ "5k23/SLS0RS0pVD9Kudl8miQSUXbBljoD5wru1rAUOiTySCUWP9dv5FyGobYyBCCJUluG4bC8IiH\n"
-									+ "stDSUBh6";
-	
+			+ "CROQYNzdbJTx4CVX+NBM7Ysa/QkQJ7AavaD4N62fAB+8eC/gcfc0ICBsrbJSc6lA9JoxkhQw0JZq\n"
+			+ "2g1OxmbRY6fddC2t5B0BaCyQ9+XFTmgmz0hQSuYehfOzZpXGjOLMk9rMsgoq213Pk8mXAgMBAAEC\n"
+			+ "gYA9fi/khE0f8AGtdoeJpYiGY5aa7DxeM8iwsOE5M2+hLiDiZNrofPrWHCoukEcImDiYBeK+fepH\n"
+			+ "fBtt8VntcQIR+Fg/4G7yM4T8cF6IoHN8ygjXtfUlen8U0tF5Zoje1/sCnkEtXFcnLT57RPqE+1/k\n"
+			+ "YTb+JlxCRBab+tcuS8j5SQJBAOWdXh+WRHxa9Y0wN3l6KZtbH4ZXMXYwWkR89ezlwMX4epDaVHIg\n"
+			+ "1SonW0x5qyfp1pBsmkO+Ktb9To1v9mJgOrsCQQCipfk0Onmaki/C3/q4guqXNF1jMMHW/dkVg/1L\n"
+			+ "lv1/ttf1pzAYUKwsmeBvCql6dK17lB330kfS2qS2SmTQSATVAkANNWJGuuQxqyHY/18Rk9902mcT\n"
+			+ "2Uw1Gk73BaE4AXd3a/XRA148OntIs37jBVS9NQxsvnKZVwUr3OJ57Gjl+9clAkAkb/5mPvtjL4DX\n"
+			+ "rYnjsCCSAz8wq4mhenkZotoqBwd/hxzWgb/6kogf7dEjz0Wsk1sSJlqYXFmO2UCxPfVb2+aJAkB2\n"
+			+ "5k23/SLS0RS0pVD9Kudl8miQSUXbBljoD5wru1rAUOiTySCUWP9dv5FyGobYyBCCJUluG4bC8IiH\n" + "stDSUBh6";
+
 	/** 默认AES密钥 */
 	private static final String AES_KEY = "anYX2bSM6l/z7OSSox6zKA==\n";
-	
+
 	private static String toHexString(byte[] b) {
 		StringBuilder sb = new StringBuilder(b.length * 2);
 		for (int i = 0; i < b.length; i++) {
@@ -39,7 +45,7 @@ public class Crypter {
 		}
 		return sb.toString();
 	}
-	
+
 	/**
 	 * 使用RSA算法的公钥加密明文字符串，经过Base64后输出
 	 * @param plaintext 明文字符串
@@ -55,7 +61,7 @@ public class Crypter {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 使用RSA算法的私钥解密密文字符串
 	 * @param ciphertext 经过Base64后的密文字符串
@@ -87,7 +93,7 @@ public class Crypter {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 进行BASE64编码
 	 * @param input 待编码字符串
@@ -119,7 +125,7 @@ public class Crypter {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 进行AES算法加密
 	 * @param input 待加密字符串
@@ -128,7 +134,7 @@ public class Crypter {
 	public static String encryptAES(String input) {
 		return encryptAES(input, AES_KEY);
 	}
-	
+
 	/**
 	 * 进行AES算法加密
 	 * @param input 待加密字符串
@@ -145,7 +151,7 @@ public class Crypter {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 进行AES算法解密
 	 * @param input 待解密字符串
@@ -154,7 +160,7 @@ public class Crypter {
 	public static String decryptAES(String input) {
 		return decryptAES(input, AES_KEY);
 	}
-	
+
 	/**
 	 * 进行AES算法解密
 	 * @param input 待解密字符串
@@ -170,5 +176,76 @@ public class Crypter {
 			logger.error("decryptAES error.", e);
 		}
 		return null;
+	}
+
+	/**
+	 * AES/CBC/PKCS5Padding 模式的加密
+	 * @param input 明文 
+	 * @param key 密钥，向量iv也使用该值
+	 * @return String 密文
+	 */
+	public static String encryptAESonCBC(String input, String key) {
+		// 判断Key是否正确    
+		if (StringOperate.isRealNull(key)) {
+			logger.error("Key不可以为空");
+			return null;
+		}
+		// 判断Key是否为16位    
+		if (key.length() != 16) {
+			logger.error("Key长度不是16位");
+			return null;
+		}
+
+		try {
+			byte[] raw = key.getBytes();
+			SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding"); // "算法/模式/补码方式"    
+			IvParameterSpec iv = new IvParameterSpec(raw); // 使用CBC模式，需要一个向量iv，可增加加密算法的强度    
+			cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+			byte[] encrypted = cipher.doFinal(input.getBytes());
+
+			return Base64.encodeBase64String(encrypted); // 此处使用BAES64做转码功能，同时能起到2次加密的作用。    
+		} catch (Exception e) {
+			logger.error("encryptAESonCBC error.", e);
+		}
+		return null;
+	}
+
+	/**
+	 * AES/CBC/PKCS5Padding 模式的解密
+	 * @param input 密文 
+	 * @param key 密钥，向量iv也使用该值
+	 * @return String 明文
+	 */
+	public static String decryptAESonCBC(String input, String key) {
+		// 判断Key是否正确    
+		if (StringOperate.isRealNull(key)) {
+			logger.error("Key不可以为空");
+			return null;
+		}
+		// 判断Key是否为16位    
+		if (key.length() != 16) {
+			logger.error("Key长度不是16位");
+			return null;
+		}
+		try {
+			byte[] raw = key.getBytes("ASCII");
+			SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			IvParameterSpec iv = new IvParameterSpec(key.getBytes());
+			cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+			byte[] encrypted1 = Base64.decodeBase64(input); // 先用Base64解密    
+			try {
+				byte[] original = cipher.doFinal(encrypted1);
+				String originalString = new String(original);
+				return originalString;
+			} catch (Exception e) {
+				System.out.println(e.toString());
+				return null;
+			}
+		} catch (Exception e) {
+			logger.error("decryptAESonCBC error.", e);
+			return null;
+		}
 	}
 }
