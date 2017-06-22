6f1dce298128cf3ccdab811ac822be7c51798d90
ZongJie <zongjie@novelbio.com>
2017-06-22 20:27:14 +0800

    1. 修正异常

--- base/src/main/java/com/novelbio/base/ExceptionNbcParamError.java
+++ base/src/main/java/com/novelbio/base/ExceptionNbcParamError.java
@@ -4,4 +4,7 @@ public class ExceptionNbcParamError extends RuntimeException {
 	public ExceptionNbcParamError(String msg) {
 		super(msg);
 	}
+	public ExceptionNbcParamError(String msg, Throwable t) {
+		super(msg, t);
+	}
 }
