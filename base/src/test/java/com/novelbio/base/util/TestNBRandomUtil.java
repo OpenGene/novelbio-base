7c710bc6a07b5d18b77e50605b547195e23cb28a
liqi <qier4536@163.com>
2018-06-11 09:42:38 +0800

    添加使用UUID获取随机字符串

--- base/src/test/java/com/novelbio/base/util/TestNBRandomUtil.java
+++ base/src/test/java/com/novelbio/base/util/TestNBRandomUtil.java
@@ -36,14 +36,46 @@ public class TestNBRandomUtil {
 	@Test
 	public void testGetString() {
 		int maxInt = 15; // 字符长度
-		int loopCnt = 10; //	每个长度循环次数
+		int loopCnt = 10; // 每个长度循环次数
 		for (int i = 1; i <= maxInt; i++) {
-			for (int j =0; j< loopCnt ; j++) {
+			for (int j = 0; j < loopCnt; j++) {
 				String strInt = NBRandomUtil.getString(i);
-//				System.out.println(strInt);
+				// System.out.println(strInt);
 				assertThat(strInt.length() == i);
 			}
 		}
+	}
+
+	@Test
+	public void testGetUUIDStr() {
+
+		int len1 = 0;
+		try {
+			String str = NBRandomUtil.getUUIDString(len1);
+			fail();
+		} catch (Exception e) {
+			if (e instanceof IllegalArgumentException) {
+				assertTrue(true);
+			} else {
+				fail();
+			}
+		}
+
+		int len2 = 1;
+		String str2 = NBRandomUtil.getUUIDString(len2);
+		assertThat(str2.length() == len2);
+
+		int len3 = 31;
+		String str3 = NBRandomUtil.getUUIDString(len3);
+		assertThat(str3.length() == len3);
+
+		int len4 = 32;
+		String str4 = NBRandomUtil.getUUIDString(len4);
+		assertThat(str4.length() == len4);
+
+		int len5 = 33;
+		String str5 = NBRandomUtil.getUUIDString(len5);
+		assertThat(str5.length() == 32);
 	}
 
 }
