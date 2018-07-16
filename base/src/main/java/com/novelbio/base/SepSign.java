4e3537fde1393a07cf33846ed030856c717d47bf
fanxiaolong <fanxiaolong2314@gmail.com>
2018-07-16 22:13:31 +0800

    修正一个连接符.

--- base/src/main/java/com/novelbio/base/SepSign.java
+++ base/src/main/java/com/novelbio/base/SepSign.java
@@ -52,7 +52,7 @@ public class SepSign {
 	public static final String SEP_INFO_WELLS = "#";
 
 	/** 创建物种索引任务ID的分隔符 */
-	public static final String TASK_INDEX_ID_SEP = "-";
+	public static final String TASK_INDEX_ID_SEP = "--";
 	
 	/** subTask的样本名/组名的后缀 */
 	public static final String SEP_SUBTASK_SUFFIX = ".";
