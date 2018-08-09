dca92e355b43caebea6473edba0f90214d334a88
ZongJie <zongjie@novelbio.com>
2018-08-09 21:26:10 +0800

    1. 上传Alignment

--- base/src/main/java/com/novelbio/base/dataStructure/Alignment.java
+++ base/src/main/java/com/novelbio/base/dataStructure/Alignment.java
@@ -2,6 +2,8 @@ package com.novelbio.base.dataStructure;
 
 import java.util.Comparator;
 
+import com.novelbio.base.StringOperate;
+
 public interface Alignment {
 	int getStartAbs();
 	int getEndAbs();
@@ -27,5 +29,42 @@ public interface Alignment {
 			}
 		}
 		
+	}
+	
+	/** 判断Align1是否cover Align2 */
+	public static boolean isAlignCoverAnother(Alignment align1, Alignment align2) {
+		return align1.getStartAbs() <= align2.getStartAbs() && align1.getEndAbs() >= align2.getEndAbs();
+	}
+	
+	/** 判断两个align是否overlap */
+	public static boolean isOverlap(Alignment align1, Alignment align2) {
+		return align1.getStartAbs() <= align2.getEndAbs() && align1.getEndAbs() >= align2.getStartAbs();
+	}
+	
+	/** 判断两个align是否overlap 不考虑头尾相连的情况，意思不是 >= 和 <= */
+	public static boolean isSiteInAlign(Alignment align, int site) {
+		return site >= align.getStartAbs() && site <= align.getEndAbs();
+	}
+
+	public static int overlapLen(Alignment align1, Alignment align2) {
+		validateRef(align1, align2);
+		if (!isOverlap(align1, align2)) {
+			return 0;
+		}
+		int refStart = align1.getStartAbs();
+		int refEnd = align1.getEndAbs();
+		
+		int altStart = align2.getStartAbs();
+		int altEnd = align2.getEndAbs();
+		
+		int len = Math.min(refEnd, altEnd) - Math.max(refStart, altStart);
+		return len;
+	}
+	
+	static void validateRef(Alignment align1, Alignment align2) {
+		//这个报错应该不会出现
+		if (!StringOperate.isEqual(align1.getRefID(), align2.getRefID())) {
+			throw new RuntimeException("refId is differ " + align1.getRefID() + " " + align2.getRefID());
+		}
 	}
 }
